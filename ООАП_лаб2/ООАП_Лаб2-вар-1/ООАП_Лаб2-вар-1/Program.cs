9e199dd5f9af46a1e793facdfccbc1dd43aa7c8e
Oleksandr <117659537+AlexSkul@users.noreply.github.com>
2023-10-30 23:58:25 +0200

    Add files via upload

---
+++ ООАП_лаб2/ООАП_Лаб2-вар-1/ООАП_Лаб2-вар-1/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class JewelryItem
+{
+    public abstract string Material { get; }
+    public abstract double Weight { get; }
+    public abstract double Price { get; }
+    public abstract string Name { get; } 
+}
+
+class GoldJewelryItem : JewelryItem
+{
+    public override string Material => "Золото";
+    public override double Weight { get; }
+    public override double Price { get; }
+    public override string Name { get; } 
+
+    public GoldJewelryItem(string name, double weight, double price)
+    {
+        Name = name;
+        Weight = weight;
+        Price = price;
+    }
+}
+
+class SilverJewelryItem : JewelryItem
+{
+    public override string Material => "Срібло";
+    public override double Weight { get; }
+    public override double Price { get; }
+    public override string Name { get; } 
+
+    public SilverJewelryItem(string name, double weight, double price)
+    {
+        Name = name;
+        Weight = weight;
+        Price = price;
+    }
+}
+
+abstract class JewelryFactory
+{
+    public abstract JewelryItem CreateItem(string name, double weight, double price);
+}
+
+class GoldJewelryFactory : JewelryFactory
+{
+    public override JewelryItem CreateItem(string name, double weight, double price)
+    {
+        return new GoldJewelryItem(name, weight, price);
+    }
+}
+
+class SilverJewelryFactory : JewelryFactory
+{
+    public override JewelryItem CreateItem(string name, double weight, double price)
+    {
+        return new SilverJewelryItem(name, weight, price);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<JewelryItem> goldCatalog = new List<JewelryItem>();
+        List<JewelryItem> silverCatalog = new List<JewelryItem>();
+
+        while (true)
+        {
+            Console.WriteLine("Оберіть дію:");
+            Console.WriteLine("1. Переглянути каталог");
+            Console.WriteLine("2. Додати новий виріб");
+            Console.WriteLine("3. Вийти");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                DisplayCatalog(goldCatalog, silverCatalog);
+            }
+            else if (choice == "2")
+            {
+                AddNewJewelryItem(goldCatalog, silverCatalog);
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine("Дякуємо за використання програми!");
+                break;
+            }
+        }
+    }
+
+    static void DisplayCatalog(List<JewelryItem> goldCatalog, List<JewelryItem> silverCatalog)
+    {
+        Console.WriteLine("Оберіть каталог для перегляду:");
+        Console.WriteLine("1. Золото");
+        Console.WriteLine("2. Срібло");
+
+        string catalogChoice = Console.ReadLine();
+
+        if (catalogChoice == "1")
+        {
+            Console.WriteLine("Каталог 'Золото':");
+            foreach (var item in goldCatalog)
+            {
+                Console.WriteLine($"Назва: {item.Name}");
+                Console.WriteLine($"Матеріал: {item.Material}");
+                Console.WriteLine($"Вага: {item.Weight} г");
+                Console.WriteLine($"Ціна: ${item.Price}");
+                Console.WriteLine();
+            }
+        }
+        else if (catalogChoice == "2")
+        {
+            Console.WriteLine("Каталог 'Срібло':");
+            foreach (var item in silverCatalog)
+            {
+                Console.WriteLine($"Назва: {item.Name}");
+                Console.WriteLine($"Матеріал: {item.Material}");
+                Console.WriteLine($"Вага: {item.Weight} г");
+                Console.WriteLine($"Ціна: ${item.Price}");
+                Console.WriteLine();
+            }
+        }
+    }
+
+    static void AddNewJewelryItem(List<JewelryItem> goldCatalog, List<JewelryItem> silverCatalog)
+    {
+        Console.WriteLine("Додати новий виріб:");
+        Console.WriteLine("Оберіть категорію:");
+        Console.WriteLine("1. Золото");
+        Console.WriteLine("2. Срібло");
+
+        string categoryChoice = Console.ReadLine();
+        Console.Write("Введіть назву виробу: ");
+        string name = Console.ReadLine();
+
+        if (categoryChoice == "1")
+        {
+            Console.Write("Введіть вагу золотого виробу (г): ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введіть ціну золотого виробу ($): ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            JewelryFactory goldFactory = new GoldJewelryFactory();
+            JewelryItem goldItem = goldFactory.CreateItem(name, weight, price);
+            goldCatalog.Add(goldItem);
+            Console.WriteLine("Золотий виріб додано до каталогу.");
+        }
+        else if (categoryChoice == "2")
+        {
+            Console.Write("Введіть вагу срібного виробу (г): ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введіть ціну срібного виробу ($): ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            JewelryFactory silverFactory = new SilverJewelryFactory();
+            JewelryItem silverItem = silverFactory.CreateItem(name, weight, price);
+            silverCatalog.Add(silverItem);
+            Console.WriteLine("Срібний виріб додано до каталогу.");
+        }
+    }
+}
---
+++ ООАП_лаб2/ООАП_Лаб2-вар4/ООАП_Лаб2-вар4/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+class CarConfiguration
+{
+    public string EngineType { get; set; }
+    public double EngineVolume { get; set; }
+    public bool HasABS { get; set; }
+    public bool HasESP { get; set; }
+    public int SafetyAirbags { get; set; }
+    public bool HasComputer { get; set; }
+    public bool HasAC { get; set; }
+    public string InteriorTrim { get; set; }
+
+    public void ShowConfiguration()
+    {
+        Console.WriteLine($"Двигун: {EngineType}, Об'єм двигуна: {EngineVolume} л");
+        Console.WriteLine($"ABS: {HasABS}, ESP: {HasESP}");
+        Console.WriteLine($"Подушки безпеки: {SafetyAirbags}");
+        Console.WriteLine($"Бортовий комп'ютер: {HasComputer}");
+        Console.WriteLine($"Кондиціонер: {HasAC}");
+        Console.WriteLine($"Обшивка салону: {InteriorTrim}");
+    }
+}
+
+class CarConfigurationBuilder
+{
+    private CarConfiguration carConfiguration = new CarConfiguration();
+
+    public CarConfigurationBuilder SetEngine(string engineType, double engineVolume)
+    {
+        carConfiguration.EngineType = engineType;
+        carConfiguration.EngineVolume = engineVolume;
+        return this;
+    }
+
+    public CarConfigurationBuilder SetSafetyFeatures(bool hasABS, bool hasESP, int safetyAirbags)
+    {
+        carConfiguration.HasABS = hasABS;
+        carConfiguration.HasESP = hasESP;
+        carConfiguration.SafetyAirbags = safetyAirbags;
+        return this;
+    }
+
+    public CarConfigurationBuilder SetAdditionalFeatures(bool hasComputer, bool hasAC)
+    {
+        carConfiguration.HasComputer = hasComputer;
+        carConfiguration.HasAC = hasAC;
+        return this;
+    }
+
+    public CarConfigurationBuilder SetInterior(string interiorTrim)
+    {
+        carConfiguration.InteriorTrim = interiorTrim;
+        return this;
+    }
+
+    public CarConfiguration Build()
+    {
+        return carConfiguration;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Оберіть комплектацію автомобіля:");
+        Console.WriteLine("1. Базова");
+        Console.WriteLine("2. Стандарт");
+        Console.WriteLine("3. Покращена");
+        Console.WriteLine("4. Преміум");
+        Console.Write("Ваш вибір (1-4): ");
+
+        int userChoice;
+        if (int.TryParse(Console.ReadLine(), out userChoice) && userChoice >= 1 && userChoice <= 4)
+        {
+            CarConfigurationBuilder builder = new CarConfigurationBuilder();
+
+            switch (userChoice)
+            {
+                case 1:
+                    builder.SetEngine("Бензин", 1.6)
+                           .SetSafetyFeatures(false, false, 4)
+                           .SetAdditionalFeatures(false, false)
+                           .SetInterior("Тканина");
+                    break;
+                case 2:
+                    builder.SetEngine("Бензин", 2.0)
+                           .SetSafetyFeatures(true, true, 6)
+                           .SetAdditionalFeatures(true, false)
+                           .SetInterior("Шкіра");
+                    break;
+                case 3:
+                    builder.SetEngine("Дизель", 2.0)
+                           .SetSafetyFeatures(true, true, 6)
+                           .SetAdditionalFeatures(true, true)
+                           .SetInterior("Шкіра");
+                    break;
+                case 4:
+                    builder.SetEngine("Дизель", 3.0)
+                           .SetSafetyFeatures(true, true, 8)
+                           .SetAdditionalFeatures(true, true)
+                           .SetInterior("Шкіра з підігрівом");
+                    break;
+            }
+
+            CarConfiguration carConfig = builder.Build();
+            Console.WriteLine("Характеристики обраної комплектації автомобіля:");
+            carConfig.ShowConfiguration();
+
+            double carPrice = CalculateCarPrice(carConfig);
+            Console.WriteLine($"Загальна вартість: {carPrice} грн");
+        }
+        else
+        {
+            Console.WriteLine("Невірний вибір. Будь ласка, виберіть опцію від 1 до 4.");
+        }
+    }
+
+    static double CalculateCarPrice(CarConfiguration carConfig)
+    {
+        double basePrice = 20000;
+        double pricePerEngineVolume = 1000;
+        double pricePerAirbag = 500;
+        double pricePerAC = 1000;
+
+        double totalPrice = basePrice + (carConfig.EngineVolume * pricePerEngineVolume) +
+                           (carConfig.SafetyAirbags * pricePerAirbag);
+        if (carConfig.HasAC)
+        {
+            totalPrice += pricePerAC;
+        }
+
+        return totalPrice;
+    }
+}
